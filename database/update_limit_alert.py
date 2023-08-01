804915da173bfeae9c8e17bc8ea26914326c3574
Semyon <b-semen-b@mail.ru>
2023-08-01 09:11:47 +0300

    Hotfix

--- database/update_limit_alert.py
+++ database/update_limit_alert.py
@@ -5,7 +5,7 @@ def change_limit_alert(tg_id: str, last_update: str, phone: str):
     con = get_database_connection()
     cur = con.cursor()
     cur.execute(
-        f"UPDATE limits_aler    `t SET tg_id = '{tg_id}' AND last_update = '{last_update}' AND phone = '{phone}'"
+        f"UPDATE limits_alert SET tg_id = '{tg_id}' AND last_update = '{last_update}' AND phone = '{phone}'"
     )
     con.commit()
     cur.close()
